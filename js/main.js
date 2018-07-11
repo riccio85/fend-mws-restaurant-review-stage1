42faef37bf0168fc445f950cc2a0ffb9bf8e9665
riccio85 <rihanna.ke@gmail.com>
2018-07-12 00:08:54 +0200

    restaurant list styling

--- js/main.js
+++ js/main.js
@@ -175,10 +175,20 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
-  const more = document.createElement('a');
-  more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+//TODO cancellare -- non so se meglio quello come button oppure quello href per l accessibility
+  // const more = document.createElement('a');
+  // more.innerHTML = 'View Details';
+  // more.href = DBHelper.urlForRestaurant(restaurant);
+  // li.append(more)
+
+ /* fixed to button instead od a for the accessibility*/
+  const more = document.createElement('button');
+  const moreText = document.createTextNode( 'View Details');
+  more.appendChild(moreText);
+  more.addEventListener("click", (e) =>
+    window.location.href=DBHelper.urlForRestaurant(restaurant)
+  , false);
+  li.append(more);
 
   return li
 }
